6dfca35458f26c07424c4c12d2dfe69350fc8009
minh832000 <72727613+minh832000@users.noreply.github.com>
2021-10-13 15:32:18 +0700

    Edit base url

--- php/src/path.php
+++ php/src/path.php
@@ -1,4 +1,4 @@
 <?php
 
 define("ROOT_PATH", realpath(dirname(__FILE__)));
-define("BASE_URL", "http://localhost:8000/");
+define("BASE_URL", "http://ec2-18-191-64-145.us-east-2.compute.amazonaws.com/");
