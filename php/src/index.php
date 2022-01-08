6245c993cc7cce37ff5b0e81e3564dbc28729e86
minh832000 <72727613+minh832000@users.noreply.github.com>
2022-01-08 20:07:36 +0700

    demo
    lan demo

--- php/src/index.php
+++ php/src/index.php
@@ -29,7 +29,7 @@ include("path.php");
         <div class="header">
             <a href="<?php echo BASE_URL ?>" class="thehours-logo">
                 <span class="main-title">TheHours</span>
-                <span class="sub-title">Demo Đoàn Vũ Lân</span>
+                <span class="sub-title">08012022</span>
             </a>
         </div>
         <!-- END header -->
